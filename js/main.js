0edde5e19d4ece096edc763de60b364d742b78bf
maki0812 <samnel2180@yahoo.co.jp>
2022-05-16 17:39:06 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//問い合わせフォームへスクロール
+
+$(function () {
+  $('a[href^=#]').click(function () {
+    var speed = 200; // スクロール速度(ミリ秒)
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
+
+//問い合わせフォームバリデーション
